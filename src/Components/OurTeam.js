05f9d02506827108c4a98133dd01d09f0a90aae3
Rohit Ranjan <ranjanrohit812@gmail.com>
2023-01-02 12:40:32 +0530

    Video file

--- src/Components/OurTeam.js
+++ src/Components/OurTeam.js
@@ -5,24 +5,19 @@ import VS from "../../public/Assets/vs.jpg";
 import RR from "../../public/Assets/rr.jpg";
 import RM from "../../public/Assets/rm.jpg";
 import FS from "../../public/Assets/fs.jpg";
-import video from "../../public/Assets/bkg_vid.mp4";
 
 const Team = () => {
   return (
     <section className="text-gray-600 body-font px-10">
-      <div>
-        <video
-          autoplay
-          playsInline
-          loop
-          muted
-          class="css-1oe2cao"
-        >
-          <source src="../../public/Assets/bkg_vid.mp4"
-          type="video/mp4"/>
-        </video>
-      </div>
       <div className="container px-5 py-24 mx-auto">
+        <video
+          className="video w-full object-cover rounded-lg"
+          src="https://klima.com/_next/static/videos/about_1920x1080-aacff1d86bd792c26b4b15db748d0c6c.webm"
+          loop
+          autoPlay
+        >
+          Your browser does not support the video tag.
+        </video>
         <div className="flex flex-col text-center w-full mb-20 ">
           <h1 className="text-3xl sm:text-7xl font-medium title-font mb-4 text-gray-800 tracking-widest">
             OUR TEAM
