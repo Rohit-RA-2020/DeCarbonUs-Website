43eed9dabfdd8617267b6a59f52e5903de0c2d79
Rohit Ranjan <ranjanrohit812@gmail.com>
2023-05-13 01:58:37 +0530

    Tracks Added

---
+++ pages/tracks.js
@@ -0,0 +1,205 @@
+import Layout from "../src/Layout/Layout";
+import Image from "next/image";
+import FS from "../public/Assets/foot.png";
+
+const Tracks = () => {
+    return (
+        <Layout>
+            <section class="text-gray-600 body-font">
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="text-center mb-20">
+                        <Image
+                            alt="Rohit Ranjan"
+                            className="h-100 w-auto max-w-full rounded-full"
+                            src={FS}
+                        />
+                        <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">TRANSPORT</h1>
+                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Greenhouse gas (GHG) emissions from transportation account for about 27 percent of total U.S. greenhouse gas emissions, making it the largest contributor of U.S. GHG emissions.</p>
+                    </div>
+                    <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Encouraging the use of public transportation</span>
+                            </div>
+                        </div>
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Promoting the use of electric or hybrid vehicles</span>
+                            </div>
+                        </div>
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Investing in infrastructure for biking and walking</span>
+                            </div>
+                        </div>
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Telecommuting and other forms of remote work</span>
+                            </div>
+                        </div>
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Carpooling or bike sharing systems</span>
+                            </div>
+                        </div>
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Donate to government organisations for research and development to reduce carbon emissions</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="text-center mb-20">
+                        <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">EATING HABITS/FOOD</h1>
+                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">By making simple changes like eating fewer animal products, using less plastic, eating more fresh produce, and decreasing your food waste, you can cut your personal greenhouse gas emissions significantly. Keep in mind that efforts that seem small can make a big difference.</p>
+                    </div>
+                    <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Eating a plant-based diet</span>
+                            </div>
+                        </div>
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Choosing locally-sourced and in-season foods</span>
+                            </div>
+                        </div>
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Reducing food waste</span>
+                            </div>
+                        </div>
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Using reusable containers and bags</span>
+                            </div>
+                        </div>
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Cooking at home instead of eating out</span>
+                            </div>
+                        </div>
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Investing in a home garden, or supporting community gardens</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="container px-5 py-24 mx-auto">
+                    <div class="text-center mb-20">
+                        <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">HOUSEHOLD</h1>
+                        <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">Many of our daily activities cause emissions of greenhouse gases. For example, we produce greenhouse gas emissions from burning gasoline when we drive, burning oil or gas for home heating, or using electricity generated from coal, natural gas, and oil. Greenhouse gas emissions vary among individuals depending on a person's location, habits, and personal choices.</p>
+                    </div>
+                    <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Using energy-efficient appliances and light bulbs</span>
+                            </div>
+                        </div>
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Installing solar panels or other forms of renewable energy</span>
+                            </div>
+                        </div>
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Planting trees or installing shading devices</span>
+                            </div>
+                        </div>
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Using a programmable thermostat to control the temperature
+                                </span>
+                            </div>
+                        </div>
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Turning off lights, appliances and electronics when not in use</span>
+                            </div>
+                        </div>
+                        <div class="p-2 sm:w-1/2 w-full">
+                            <div class="bg-gray-100 rounded flex p-4 h-full items-center">
+                                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" class="text-green-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+                                    <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                                    <path d="M22 4L12 14.01l-3-3"></path>
+                                </svg>
+                                <span class="title-font font-medium">Recycling and composting household waste</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </Layout>
+    );
+}
+export default Tracks;
